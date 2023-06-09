4a16ee3e0e2852655739ce9a01d5426d9a5b9f36
Fafemi Adeola <adeola5678@gmail.com>
2023-06-09 01:05:23 +0100

    fix: nodes

--- src/helpers.js
+++ src/helpers.js
@@ -8,33 +8,46 @@ export const truncateAddress = (address) => {
 };
 
 export const fetchData = async (publicKey) => {
-
-  let nodeList = await nodeList();
+  let nodeList = await getNodeList();
 
   for (let i = 0; i < nodeList.length; i++) {
-  try {
-    const response = await axios.get(`${nodeList[i]}/task/6FgtEX6qd4XCuycUfJGuJTr41qcfvM59ueV2L17eSdan/linktree/get/${publicKey}`);
-    if (response.data && response.data.data) {
-      console.log(`Data found in node ${i}:`, response.data.data);
-      const userData = response.data.data.linktree;
-      return userData;
+    try {
+      const response = await axios.get(
+        `${nodeList[i]}/task/6FgtEX6qd4XCuycUfJGuJTr41qcfvM59ueV2L17eSdan/linktree/get/${publicKey}`
+      );
+      if (response.data && response.data.data) {
+        console.log(`Data found in node ${i}:`, response.data.data);
+        const userData = response.data.data.linktree;
+        return userData;
+      }
+    } catch (error) {
+      return "Error";
     }
-  } catch (error) {
-    return 'Error'
   }
-}
   return "Error";
-
 };
 
 export const getNodeList = async () => {
-  let nodeList = []
-  let nodeResponse = await axios.get(
-    'https://tasknet.koii.live/nodes/6FgtEX6qd4XCuycUfJGuJTr41qcfvM59ueV2L17eSdan'
-  );
-  for (let i = 0; i < nodeResponse.data.length; i++) {
-    nodeList.push(nodeResponse.data[i].data.url);
+  let nodeList = [];
+  const fallbackNodes = [
+    "https://tasknet.koii.live/task/6FgtEX6qd4XCuycUfJGuJTr41qcfvM59ueV2L17eSdan",
+    "https://tasknet-ports-2.koii.live/task/6FgtEX6qd4XCuycUfJGuJTr41qcfvM59ueV2L17eSdan",
+    "https://tasknet-ports-2.koii.live/task/6FgtEX6qd4XCuycUfJGuJTr41qcfvM59ueV2L17eSdan",
+  ];
+
+  try {
+    let nodeResponse = await axios.get(
+      "https://tasknet.koii.live/nodes/6FgtEX6qd4XCuycUfJGuJTr41qcfvM59ueV2L17eSdan"
+    );
+    for (let i = 0; i < nodeResponse.data.length; i++) {
+      nodeList.push(nodeResponse.data[i].data.url);
     }
-  console.log(nodeList)
+  } catch (error) {
+    console.error(
+      "Failed to fetch node list from primary node. Falling back to fallback nodes..."
+    );
+    nodeList = fallbackNodes;
+  }
+
   return nodeList;
-}
+};
