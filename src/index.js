a116ba84ba8c799b4ff51f6593e60a42e930e46e
Fafemi Adeola <adeola5678@gmail.com>
2023-06-09 12:08:32 +0100

    fix: build

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { WalletContextProvider } from "./contexts";
-import LinksComponent from "./pages/LinksComponent";
 import "@rainbow-me/rainbowkit/styles.css";
-import CreateLinktree from "./pages/CreateLinktree";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import ToggleThemeMode from "./ToggleThemeMode";
